e3916c60b3561705169b5eb919314b779584dcf9
Fernando Fukunaga <fernandosantanna.2457@aluno.saojudas.br>
2023-05-19 14:44:56 -0300

    criando ConnectionFactory com o mysql e configurando o pom.xml

---
+++ src/main/java/br/com/ecosolucoes/ecotroca/ConnectionFactory.java
@@ -0,0 +1,25 @@
+package br.com.ecosolucoes.ecotroca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    private String user = "root";
+    private String senha = "";
+    private String host = "localhost";
+    private String porta = "3306";
+    private String banco = "ecotroca";
+    private String timezone = "&useTimezone=true&serverTimezone=America/Sao_Paulo";
+    
+    public Connection obterConexao() {
+        try {
+        Connection c = DriverManager.getConnection("jdbc:mysql://"+host+":"+porta+"/"+banco+"?user="+user+"&password="+senha+timezone);
+        return c;
+        }
+        
+        catch (Exception e) {
+        e.printStackTrace();
+        return null;
+        }
+    }
+}
