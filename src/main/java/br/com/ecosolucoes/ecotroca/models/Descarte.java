06e1c1801c300e4c9303edc027fc9f36702d8217
Sara Alves <sarinhaacordeiro@gmail.com>
2023-05-28 23:54:27 -0300

    criei classe cidade e descarte

---
+++ src/main/java/br/com/ecosolucoes/ecotroca/models/Descarte.java
@@ -0,0 +1,65 @@
+package br.com.ecosolucoes.ecotroca.models;
+import java.time.LocalDate;
+
+
+public class Descarte {
+    
+    private int id;
+    private int idCidadao;
+    private int idUsuario;
+    private LocalDate dataHoraDescarte;
+    private double pesoTotalDescarte;
+    private double totalPontosGerados;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCidadao() {
+        return idCidadao;
+    }
+
+    public void setIdCidadao(int idCidadao) {
+        this.idCidadao = idCidadao;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public LocalDate getDataHoraDescarte() {
+        return dataHoraDescarte;
+    }
+
+    public void setDataHoraDescarte(LocalDate dataHoraDescarte) {
+        this.dataHoraDescarte = dataHoraDescarte;
+    }
+
+    public double getPesoTotalDescarte() {
+        return pesoTotalDescarte;
+    }
+
+    public void setPesoTotalDescarte(double pesoTotalDescarte) {
+        this.pesoTotalDescarte = pesoTotalDescarte;
+    }
+
+    public double getTotalPontosGerados() {
+        return totalPontosGerados;
+    }
+
+    public void setTotalPontosGerados(double totalPontosGerados) {
+        this.totalPontosGerados = totalPontosGerados;
+    }
+    
+    
+    
+    
+}
